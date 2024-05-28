80a050ca44ba8ba34eb98122b4ccc6f20d05faf7
042224 <042224@DESKTOP-T4FDL1P>
2024-05-28 08:28:33 +0800

    添加项目文件。

---
+++ NetService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nitrans
+{
+    public static class NetService
+    {
+        private static ServiceStatus _serviceStatus = new ServiceStatus() { Status = 0, StatusName = "停止" };
+
+        /// <summary>
+        /// 服务状态
+        /// </summary>
+        public struct ServiceStatus
+        {
+            public int Status;
+            public string StatusName;
+        }
+
+        /// <summary>
+        /// 服务状态的获取或设置
+        /// </summary>
+        public static ServiceStatus Status
+        {
+            get { return _serviceStatus; }
+            set { _serviceStatus = value; }
+        }
+
+        /// <summary>
+        /// 正常情况的状态变更
+        /// </summary>
+        /// <param name="status"></param>
+        public static void ChangeServiceStatus(int status)
+        {
+            switch (status)
+            {
+                case 0:
+                    Status = new ServiceStatus() { Status = 0, StatusName = "停止" };
+                    break;
+                case 1:
+                    Status = new ServiceStatus() { Status = 1, StatusName = "启动" };
+                    break;
+                default:
+                    Status = new ServiceStatus() { Status = 0, StatusName = "停止" };
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 异常情况，显示异常原因
+        /// </summary>
+        /// <param name="status"></param>
+        /// <param name="errmsg"></param>
+        public static void ChangeServiceStatus(int status, string errmsg)
+        {
+            Status = new ServiceStatus() { Status = status, StatusName = errmsg };
+        }
+
+    }
+}
---
+++ NettyServer.cs
@@ -0,0 +1,51 @@
+﻿using DotNetty.Handlers.Logging;
+using DotNetty.Handlers.Timeout;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nitrans
+{
+    public class NettyServer
+    {
+        MultithreadEventLoopGroup bossGroup = new MultithreadEventLoopGroup(1);
+        MultithreadEventLoopGroup workGroup = new MultithreadEventLoopGroup();
+
+        public NettyServer() 
+        {
+            ServerStart(8080);
+        }
+
+        async void ServerStart(int port)
+        {
+            try
+            {
+                var bootstrap = new ServerBootstrap()
+                    .Group(bossGroup, workGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .Option(ChannelOption.SoBacklog, 100)
+                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                        pipeline.AddLast(new LoggingHandler());
+                    }));
+                IChannel boundChannel = await bootstrap.BindAsync(IPAddress.Any, port);
+            }
+            finally
+            {
+                await Task.WhenAll(
+                    bossGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)),
+                    workGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
+            }
+        }
+
+
+    }
+}
